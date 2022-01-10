1f1f6463416e5c652c2a65e8e13e087c2b24e216
Sergey <serg1557733@ukr.net>
2022-01-10 19:43:03 +0200

    init: create front/back apps

---
+++ backend/app.js
@@ -0,0 +1,54 @@
+
+const express = require('express');
+const app = express()
+const cors = require("cors");
+const http = require('http'); //create new http
+const socket = require("socket.io");
+
+
+const server = http.createServer(app);
+
+const io = require("socket.io")(server, {
+  cors: {
+    origin: "http://localhost:3000"  //client endpoint and port
+  }
+});
+
+const PORT = process.env.PORT || 5000;
+
+app.use(cors());// cors 
+
+
+
+//main page
+app.get('/', (req, res) => {
+  res.send('here will be login page')
+})
+
+//login page
+app.get('/chat', (req, res) => {
+  res.send('Chat page')
+})
+
+//on connection listen messages and send back text and user name in chat
+io.on("connection", (socket) => {
+    socket.on("message", (data) => {
+        console.log(data.message); 
+        io.emit('chat_message',{
+            message: data.message,
+            name: data.name
+        })
+      });
+      
+  console.log(`user with ID :${socket.id} , connected to socket`); 
+});
+
+
+
+
+
+//server.listen(PORT);
+
+server.listen(PORT, () => {
+  console.log(`Server started. Port: ${PORT}`)
+})
